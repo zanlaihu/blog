7e1a3132d86893e96e7572f5c5db47f86dff3a9c
胡赞來 <361206768@qq.com>
2022-04-15 22:54:18 +0800

    finish top content

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -4,46 +4,31 @@ import { GithubLogo } from '../github-logo'
 import { Logo } from '../logo'
 import './index.css'
 
-const NavigationInfo = [
-  {
-    title: "学习笔记",
-    url: PAGE_URL.NOTE
-  },
-  {
-    title: "专业技能",
-    url: PAGE_URL.SKILL
-  },
-  {
-    title: "项目经历",
-    url: PAGE_URL.PROJECT
-  },
-  {
-    title: "关于本站",
-    url: PAGE_URL.ABOUT
-  }
-]
-
 export const Navigation = () => {
   return (
-    <div className='navigation'>
-      <div className='navigation-row'>
-        <Link to={PAGE_URL.HOME} className="navigation-choice">
+    <div className='navigation-var navigation'>
+      <div className='left-content'>
+        <Link to={PAGE_URL.HOME} className='logo'>
           <Logo />
         </Link>
-        {NavigationInfo.map((item, index) => <Link to={item.url} className="navigation-choice">{item.title}</Link>)}
-        {/* <Link to={PAGE_URL.NOTE} className='navigation-choice'>
-          学习笔记
+        <Link to={PAGE_URL.NOTE} className='navigation-choice'>
+          笔记
         </Link>
         <Link to={PAGE_URL.SKILL} className='navigation-choice'>
-          专业技能
+          技能树
         </Link>
         <Link to={PAGE_URL.PROJECT} className='navigation-choice'>
           项目经历
         </Link>
         <Link to={PAGE_URL.ABOUT} className='navigation-choice'>
           关于本站
-        </Link> */}
-        <Link to={PAGE_URL.GITHUB}><GithubLogo /></Link>
+        </Link>
+      </div>
+      <div className='right-content'>
+        <a className='navigation-choice'>本站源码</a>
+        <a className='navigation-choice'>
+          <GithubLogo />
+        </a>
       </div>
     </div>
   )
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,12 +6,21 @@ import './index.css'
 export const Home = () => {
   return (
     <div className='home'>
-      <Navigation />
-      <div className='content'>
-        <div className='content-title'>因为兴趣使然而建立的小站</div>
-        <div className='content-description'>
-          用于放置学习笔记、专业技能、项目经历、及GitHub。快点击顶部的导航来选择感兴趣的内容吧！
+      <div className='top-content'>
+        <div className='top-content-inner'>
+          <div className='left-blue'></div>
+          <div className='right-blue-purple'></div>
+          <Navigation />
+          <div className='text-content'>
+            <div className='top-content-title'>因为兴趣使然而建立的小站</div>
+            <div className='content-description'>
+              用于放置学习笔记、专业技能、项目经历、及GitHub。快点击顶部的导航来选择感兴趣的内容吧！
+            </div>
+          </div>
         </div>
+      </div>
+      <div>
+        <div></div>
       </div>
     </div>
   )
