3525d5787b664b51d72dcba4f9559651fa30183d
胡赞來 <361206768@qq.com>
2022-04-16 19:47:06 +0800

    修复bug

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,15 +7,15 @@ export const Home = () => {
   return (
     <div className='home'>
       <div className='top-content'>
+        <div className='left-blue'></div>
+        <div className='right-blue-purple'></div>
         <div className='top-content-inner'>
-          <div className='left-blue'></div>
-          <div className='right-blue-purple'></div>
           <Navigation />
           <div className='text-content'>
             <div className='top-content-title'>因为兴趣使然而建立的小站</div>
             <div className='content-description'>
               本站是我的个人网站，用于放置学习过程中的笔记、个人技能以及项目经历。其中也会加入一些前端实验性质的展示。这个是使用react搭建的第二版，因为第一版是用的原生HTML：
-              <a href='https://zanlaihu.github.io/old-blog/index.html' >
+              <a href='https://zanlaihu.github.io/old-blog/index.html'>
                 旧版地址
               </a>
               ，所以想尝试用GitHub
