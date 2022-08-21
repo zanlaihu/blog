067da5e610a3ab4dea2538e60cc05cbc3c6d6123
胡赞來 <361206768@qq.com>
2022-08-22 00:28:07 +0800

    a

--- src/pages/home/components/MainContent/index.tsx
+++ src/pages/home/components/MainContent/index.tsx
@@ -4,9 +4,9 @@ export const MainContent = () => {
   return (
     <div className='home-ins home-ins-blue'>
       <div className='ins-content'>
-        <div className='ins-title'>因为兴趣使然而建立的小站</div>
+        <div className='ins-title'>本站正在使用React重构，很多网页未完成。</div>
         <div className='ins-ins'>
-          用于放置学习笔记、专业技能、项目经历、及GitHub。快点击顶部的导航来选择感兴趣的内容吧！
+          烦请访问旧网站：<a href='https://zanlaihu.github.io/old-blog/index.html'>https://zanlaihu.github.io/old-blog/index.html</a>
         </div>
         <div className='ins-link'>
           <a href='./study-notes.html'>本网站自2021年10月跳槽后，已许久没有再更新</a>
