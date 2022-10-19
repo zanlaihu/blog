0c03d49ef30c7a278daaf4854446aae5963a1fda
huzanlai <huzanlai.hzl@alibaba-inc.com>
2022-10-19 21:57:35 +0800

    ant: add

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,13 +4,19 @@ import { News } from '../../Common/components/News'
 import { MainContent } from './components/MainContent'
 import { MainItems } from './components/MainItems'
 import Bottom from './components/Bottom'
+import RotateCircle from '../../Common/components/RotateCircle'
 
 export const Home = () => {
   return (
     <div>
       <News />
       <Navigation />
-      <MainContent />
+      {/* <MainContent /> */}
+      <div className='top-bar'>
+        <div className='title-content'>欢迎来到我的站点</div>
+        <div className='title-content'>Welcome to my website</div>
+      </div>
+      <RotateCircle />
       <MainItems />
       <Bottom />
     </div>
