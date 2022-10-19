85cfa3d3db6707d10d7275a0810f94dccf834041
胡赞來 <361206768@qq.com>
2022-10-19 21:55:45 +0800

    feat: add

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,5 @@
-import { Link } from 'react-router-dom'
 import { Navigation } from '../../Common/components/Navigation'
 import { News } from '../../Common/components/News'
-import { PAGE_URL } from '../../constants/page-url/router-url'
 
 export const About = () => {
   return (
