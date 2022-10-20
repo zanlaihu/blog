0570adc0412009097b4e2962a5513699f64b3368
huzanlai <huzanlai.hzl@alibaba-inc.com>
2022-10-20 11:05:19 +0800

    a: add

--- src/Common/components/RotateCircle/index.tsx
+++ src/Common/components/RotateCircle/index.tsx
@@ -7,9 +7,17 @@ const RotateCircle: FC<RotateCircleProps> = () => {
   const circleStr =
     '</><></><></><></><></><></><></><></><></><></><></><></><></>'
 
+  const dot = ' . . / . . / '
+  const dotBlank = <div>{dot}&nbsp;&nbsp;&nbsp;&nbsp;</div>
+
   return (
     <div className='circle'>
-      <svg viewBox='0 0 100 100' width='249' height='249'>
+      <svg
+        viewBox='0 0 100 100'
+        width='250'
+        height='250'
+        className='circle-one'
+      >
         <defs>
           <path
             id='circle'
@@ -24,6 +32,43 @@ const RotateCircle: FC<RotateCircleProps> = () => {
           <textPath xlinkHref='#circle'>{circleStr}</textPath>
         </text>
       </svg>
+      <svg
+        viewBox='0 0 100 100'
+        width='400'
+        height='400'
+        className='circle-two'
+      >
+        <defs>
+          <path
+            id='circle'
+            d='
+        M 50, 50
+        m -37, 0
+        a 37,37 0 1,1 74,0
+        a 37,37 0 1,1 -74,0'
+          />
+        </defs>
+        <text font-size='4.8'>
+          <textPath xlinkHref='#circle'>
+            {dot}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{dot}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{dot}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{dot}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{dot}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{dot}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{dot}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{dot}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </textPath>
+        </text>
+      </svg>
+      {/* <svg viewBox='0 0 100 100' width='300' height='300' className='circle-three'>
+        <defs>
+          <path
+            id='circle'
+            d='
+        M 50, 50
+        m -37, 0
+        a 37,37 0 1,1 74,0
+        a 37,37 0 1,1 -74,0'
+          />
+        </defs>
+        <text font-size='7'>
+          <textPath xlinkHref='#circle'>{circleStr}</textPath>
+        </text>
+      </svg> */}
     </div>
   )
 }
