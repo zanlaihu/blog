8847591ba94eabe726f48db42c0696faaaccaa10
huzanlai <huzanlai.hzl@alibaba-inc.com>
2022-10-31 10:33:22 +0800

    at: add

--- src/components/News/index.tsx
+++ src/components/News/index.tsx
@@ -5,7 +5,7 @@ export const News = () => {
     <div className='news-content'>
       <div className='news-text'>
         <div>
-          2022年10月27日：本站新建中，目前响应式布局还没做好，建议用PC端浏览器宽屏观看哦。也可点此&nbsp;
+          2022年10月27日：本站新建中，目前移动端响应式布局还没做好，建议用PC端浏览。也可点此&nbsp;
         </div>
         <div className='new-old-ng'>进入老站</div>
       </div>
