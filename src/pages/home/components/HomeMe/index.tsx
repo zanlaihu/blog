cc21513f9b0dfbec0c65155ef6d02e0d3e614753
huzanlai <huzanlai.hzl@alibaba-inc.com>
2022-11-23 15:24:38 +0800

    update

--- src/pages/home/components/HomeMe/index.tsx
+++ src/pages/home/components/HomeMe/index.tsx
@@ -7,7 +7,7 @@ const HomeMe = () => {
     <div className='home-me-content'>
       <div className='home-me-title'>关于我</div>
       <div className='home-me-desc'>我叫胡赞来，是一名前端开发工程师。目前使用React进行C端H5开发。</div>
-      <NgWord words={'更多关于我的信息，请点击这里'} url={PAGE_URL.ME} />
+      <NgWord words={'更多关于我的信息，请点击这里'} url={PAGE_URL.ME} white/>
     </div>
   )
 }
