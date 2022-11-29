11cd36c8149b9bccd6c61039ae22f0bd72f5311b
huzanlai <huzanlai.hzl@alibaba-inc.com>
2022-11-29 19:25:01 +0800

    add

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -14,8 +14,8 @@ const Home = () => {
       <Navigation />
       <HomeTopBar />
       <HomeRecentProject />
-      <HomeMe />
       <HomeNote />
+      <HomeMe />
       <Bottom />
     </div>
   )
