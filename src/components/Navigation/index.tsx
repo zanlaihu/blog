2da40eed674ca1bc2f21e960747aa2d841df05af
huzanlai <huzanlai.hzl@alibaba-inc.com>
2022-11-30 11:24:37 +0800

    add

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -8,25 +8,19 @@ import classNames from 'classnames'
 import { useState } from 'react'
 
 const navigationList = [
-  { title: '主页', element: <Hu />, url: PAGE_URL.HOME },
-  { title: '关于我', url: PAGE_URL.ME },
-  { title: '我的项目', url: PAGE_URL.PROJECT },
-  { title: '我的笔记', outLink: 'https://www.yuque.com/klaus-rtgrx/goy4cy' },
+  { title: 'Projects', url: PAGE_URL.PROJECT },
+  { title: 'Notes', outLink: 'https://www.yuque.com/klaus-rtgrx/goy4cy' },
   // { title: '我的组件', url: PAGE_URL.COMPONENT },
-  { title: '关于本站', url: PAGE_URL.ABOUT },
-  {
-    title: '前往老站',
-    outLink: 'https://zanlaihu.github.io/old-blog/index.html',
-  },
-  {
-    title: 'GitHub',
-    element: (
-      <div className='github-middle-content'>
-        <img src={githubIcon} className='icon'></img>
-      </div>
-    ),
-    outLink: 'https://github.com/zanlaihu',
-  },
+  { title: 'About', url: PAGE_URL.ABOUT },
+  // {
+  //   title: 'GitHub',
+  //   element: (
+  //     <div className='github-middle-content'>
+  //       <img src={githubIcon} className='icon'></img>
+  //     </div>
+  //   ),
+  //   outLink: 'https://github.com/zanlaihu',
+  // },
 ]
 
 const Navigation = () => {
@@ -44,23 +38,21 @@ const Navigation = () => {
   }
 
   return (
-    <div className={classNames('ng-content', `${show && 'ng-content-black'}`)}>
+    <div className={'ng-content'}>
       <div className='ng-pc'>
         <div className='ng-pc-choice-content'>
-          {navigationList.map((item, index) => {
-            if (item.element) {
-              return (
-                <div
-                  key={index}
-                  className='ng-pc-choice'
-                  onClick={() => {
-                    ClickUrl(item)
-                  }}
-                >
-                  {item.element}
-                </div>
-              )
-            } else {
+          <div className='ng-pc-choice-left-content'>
+            <div
+              className='ng-pc-choice'
+              onClick={() => {
+                ClickUrl(PAGE_URL.HOME)
+              }}
+            >
+              <Hu />
+            </div>
+          </div>
+          <div className='ng-pc-choice-right-content'>
+            {navigationList.map((item, index) => {
               return (
                 <div
                   key={index}
@@ -72,8 +64,18 @@ const Navigation = () => {
                   {item.title}
                 </div>
               )
-            }
-          })}
+            })}
+            <div
+              className='ng-pc-choice'
+              onClick={() => {
+                ClickUrl('https://zanlaihu.github.io/old-blog/index.html')
+              }}
+            >
+              <div className='github-middle-content'>
+                <img src={githubIcon} className='icon'></img>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className='ng-mobile'>
@@ -95,7 +97,7 @@ const Navigation = () => {
           )}
         >
           <div className='ng-mobile-dropdown-line'></div>
-          {navigationList.map((item) => {
+          {navigationList.map(item => {
             if (typeof item.title === 'string') {
               return (
                 <div className='ng-mogile-dropdown-text-content'>
