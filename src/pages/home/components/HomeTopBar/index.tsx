10703aa5d246917c8b08b051d07ad41771b09e49
huzanlai <huzanlai.hzl@alibaba-inc.com>
2022-12-01 11:54:52 +0800

    add

--- src/pages/home/components/HomeTopBar/index.tsx
+++ src/pages/home/components/HomeTopBar/index.tsx
@@ -18,6 +18,7 @@ const HomeTopBar = () => {
           <div className='home-welcome-desc'>
             我在这个网站花了点小心思，用来更直观地展示个人项目、博客。目前还是以展示个人信息为主，并把最近做好的一个项目：大转盘直接放在了上面。后期项目介绍页面做好之后会更新上来。
           </div>
+          <div className='home-welcome-contact'>Contact me</div>
         </div>
       </div>
     </div>
