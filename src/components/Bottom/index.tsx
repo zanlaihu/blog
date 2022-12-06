9d45955729a16becdc81b02cd225e90d7cbbee7d
huzanlai <huzanlai.hzl@alibaba-inc.com>
2022-12-06 19:37:19 +0800

    feat: update bottom

--- src/components/Bottom/index.tsx
+++ src/components/Bottom/index.tsx
@@ -3,13 +3,15 @@ import './index.css'
 const Bottom = () => {
   return (
     <div className='bottom-content'>
-      <div className='bottom-contact'>
-        <div className='contact-title'>联系方式</div>
-        <div className='contact-message'>邮箱：klaushu@outlook.com</div>
-        <div className='contact-message'>手机：13867908869</div>
+      <div className='bottom-middle-content'>
+        <div className='bottom-contact'>
+          <div className='contact-title'>联系方式</div>
+          <div className='contact-message'>邮箱：klaushu@outlook.com</div>
+          <div className='contact-message'>手机：13867908869</div>
+        </div>
+        <div className='bottom-line'></div>
+        <div className='bottom-label'>Made with ❤️ by Zanlai.</div>
       </div>
-      <div className='bottom-line'></div>
-      <div className='bottom-label'>Made with ❤️ by Zanlai.</div>
     </div>
   )
 }
