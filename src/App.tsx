531073f5c5bed4e5cf5bdba0b0b08bc1f53d39ca
huzanlai <huzanlai.hzl@alibaba-inc.com>
2022-12-07 15:51:49 +0800

    add

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ function App() {
   return (
     <div className='App'>
       <Routes>
-        <Route path='/*' element={<Home />} />
+        <Route path={PAGE_URL.HOME} element={<Home />} />
         {/* <Route path={PAGE_URL.ME} element={<Me />} />
         <Route path={PAGE_URL.NOTE} element={<Note />} />
         <Route path={PAGE_URL.ABOUT} element={<About />} /> */}
-        <Route path='/project/*' element={<Project />} />
+        <Route path={PAGE_URL.PROJECT} element={<Project />} />
         {/* <Route path={PAGE_URL.COMPONENT} element={<MyComponent />} /> */}
       </Routes>
     </div>
--- src/constants/pageUrl/router-url.ts
+++ src/constants/pageUrl/router-url.ts
@@ -1,9 +1,4 @@
 export const PAGE_URL = {
-  HOME: '/',
-  ME: '/me',
-  ABOUT: '/about',
-  NOTE: '/note',
-  SKILL: '/skill',
-  PROJECT: '/project',
-  COMPONENT: '/component',
+  HOME: '/blog',
+  PROJECT: '/blog/project',
 }
