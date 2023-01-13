faa609779ea9ea15525d630ed2443d11c6d8725d
huzanlai <huzanlai.hzl@alibaba-inc.com>
2023-01-13 17:39:14 +0800

    add

--- src/components/TableProjectItem/index.tsx
+++ src/components/TableProjectItem/index.tsx
@@ -3,6 +3,7 @@ import './index.css'
 import leftArrow from '../../assets/img/leftArrow.svg'
 import { useNavigate } from 'react-router-dom'
 import { PAGE_URL } from '../../constants/router-url'
+import rightArrow from '../../assets/img/rightArrow.svg'
 
 interface TableProjectItemProps {
   title: string
@@ -18,10 +19,20 @@ const TableProjectItem: FC<TableProjectItemProps> = ({
   const navigate = useNavigate()
 
   return (
-    <div className='table-project-item-content'>
+    <div
+      className='table-project-item-content'
+      onClick={() => {
+        window.open(link)
+      }}
+    >
       <div className='table-project-item-title'>{title}</div>
       <div className='table-project-item-sub-title'>{subTitle}</div>
-      <div className='table-project-item-link'>Documentation</div>
+      <div className='table-project-item-link-content'>
+        <div className='table-project-item-link'>Documentation </div>
+        <div className='table-project-item-link-arrow'>
+          <img src={rightArrow}></img>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/home/components/HomeOtherProject/index.tsx
+++ src/pages/home/components/HomeOtherProject/index.tsx
@@ -16,46 +16,46 @@ const HomeOtherProject = () => {
           <TableProjectItem
             title={'大转盘'}
             subTitle={'使用大转盘进行抽奖'}
-            link={''}
+            link={'https://www.yuque.com/klaus-rtgrx/goy4cy/vphvsdbku8qf8oxp'}
           />
           <TableProjectItem
             title={'批量代发'}
             subTitle={'批量代发全称批量代发员工工资，是BO中台的功能'}
-            link={''}
+            link={'https://www.yuque.com/klaus-rtgrx/goy4cy/od46uvb6ekuzbowc'}
           />
-          <TableProjectItem
+          {/* <TableProjectItem
             title={'折扣券支付'}
             subTitle={'使用折扣券抵扣支付金额，是钱包APP的核心功能之一'}
             link={''}
-          />
-          <TableProjectItem
+          /> */}
+          {/* <TableProjectItem
             title={'券详情页'}
             subTitle={'用于展示折扣券详情的页面'}
             link={''}
-          />
-          <TableProjectItem
+          /> */}
+          {/* <TableProjectItem
             title={'活动详情页'}
             subTitle={'用于展示APP某项活动的详情页'}
             link={''}
-          />
-          <TableProjectItem
+          /> */}
+          {/* <TableProjectItem
             title={'线下扫码充值'}
             subTitle={'用于展示APP某项活动的详情页'}
             link={''}
-          />
-          <TableProjectItem
+          /> */}
+          {/* <TableProjectItem
             title={'KYC（用户信息录入）'}
             subTitle={'一步步引导用户填写个人信息的功能，是所有APP都具有的功能'}
             link={''}
-          />
-          <TableProjectItem
+          /> */}
+          {/* <TableProjectItem
             title={'钱包组件库'}
             subTitle={'为了钱包 APP 而专门开发的组件库'}
             link={''}
-          />
+          /> */}
         </div>
       </div>
-      <div className='home-other-project-title'>个人项目</div>
+      {/* <div className='home-other-project-title'>个人项目</div>
       <div className='home-other-project-sub-title'>
         这些是个人因为兴趣做的项目。
       </div>
@@ -67,7 +67,7 @@ const HomeOtherProject = () => {
             link={''}
           />
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
