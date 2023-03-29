1221c5de907a8dda3f1474acda82613dc1833ecf
胡赞來 <361206768@qq.com>
2023-03-29 21:14:24 +0800

    chore: update

--- src/constants/CH.ts
+++ src/constants/CH.ts
@@ -4,17 +4,17 @@ export const ProjectConstant = {
         title: '批量代发',
         subTitle:
           '批量代发是BO中后台的功能，用于批量代发员工工资，节省企业管理成本。',
-        link: 'https://www.yuque.com/klaus-rtgrx/goy4cy/od46uvb6ekuzbowc',
+        link: 'https://www.yuque.com/work-klaus/ulhwve/ny3t5h75yw0g8s92',
       },
       {
         title: '大转盘',
         subTitle: '钱包APP的功能之一，用于抽奖提升用户粘性。',
-        link: 'https://www.yuque.com/klaus-rtgrx/goy4cy/vphvsdbku8qf8oxp',
+        link: 'https://www.yuque.com/work-klaus/ulhwve/gn2g1q1t6scrt0bx',
       },
       {
         title: '折扣券支付',
         subTitle: '钱包APP支付的附属功能，用于在支付时抵扣总金额。',
-        link: 'https://www.yuque.com/klaus-rtgrx/goy4cy/zq8e31vbukahpz5c',
+        link: 'https://www.yuque.com/work-klaus/ulhwve/czyopgkt69crbq4n',
       },
     ]
   }
