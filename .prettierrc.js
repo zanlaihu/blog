547f1808e0427cd78a9c35c45ef26ab04e77da65
胡赞來 <361206768@qq.com>
2023-03-30 23:22:39 +0800

    chore: add prettier sort import

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,12 @@
 module.exports = {
+  printWidth: 80,
+  tabWidth: 4,
+  trailingComma: 'all',
+  singleQuote: true,
+  semi: true,
+  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
   semi: false,
   trailingComma: 'es5',
   tabWidth: 2,
