2d7e1300542a2cdd44206b4cb067f9b266c13079
胡赞來 <361206768@qq.com>
2023-03-31 09:07:01 +0800

    chore: 添加contact

--- src/components/Bottom/index.tsx
+++ src/components/Bottom/index.tsx
@@ -5,6 +5,9 @@ const Bottom = () => {
   return (
     <div className={styles.content}>
       <div className={styles.middleContent}>
+        <div className={styles.contact}>Contact</div>
+        <div className={styles.info}>电话：13867908869</div>
+        <div className={styles.info}>邮箱：klaushu@outlook.com</div>
         <div className='bottom-line'></div>
         <div className='bottom-label'>Made with ❤️ by Zanlai.</div>
       </div>
