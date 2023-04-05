5d81f9de6c4b2870fb95b8e05ec70cbc24f39a24
胡赞來 <361206768@qq.com>
2023-04-05 11:59:58 +0800

    chore: 制作projects

--- src/modules/home/AntProduct/index.tsx
+++ src/modules/home/AntProduct/index.tsx
@@ -3,15 +3,8 @@ import styles from './styles.module.scss'
 const AntProduct = () => {
   return (
     <div className={styles.content}>
-      <div className={styles.textAndPicContent}>
-        <div className={styles.textAndPic}>
-          <div className={styles.textContent}>
-            <div className={styles.textTitle}>产品化钱包</div>
-            <div className={styles.textLabel}>
-              我在蚂蚁集团工作期间，任职于蚂蚁国际-钱包科技团队。钱包科技团队的主要销售产品即“产品化钱包”。产品化钱包具有iOS和安卓两个版本，由H5统一负责业务开发，属于一个hybrid混合型应用。
-            </div>
-          </div>
-        </div>
+      <div className={styles.middleContent}>
+        <div className={styles.title}>项目经历</div>
       </div>
     </div>
   )
